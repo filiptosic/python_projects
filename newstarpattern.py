092ba7ea77936a6319353847c6b0d1b0e690bab2
Filip Tosic <tosicfilip3@gmail.com>
2022-11-19 16:08:42 -0700

    Adding 2 new star pattern projects

---
+++ newstarpattern.py
@@ -0,0 +1,15 @@
+x = int(input("enter number of rows: "))
+star = str(input("enter a symbol: "))
+pattern = star
+for i in range(x):
+    print(pattern)
+    pattern += star
+
+y = int(input("enter number of rows: "))
+pattern2 = str(input("Enter a symbol to be repeated: "))
+i = 0
+symbol = pattern2
+while i <= y:
+    print(symbol)
+    symbol += pattern2
+    i += 1
