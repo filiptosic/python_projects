ce29e92f10294f06e4ab4dd86509795cca2c6d0b
Filip Tosic <tosicfilip3@gmail.com>
2022-12-23 00:09:06 -0700

    adding useful functions module

---
+++ useful_functions.py
@@ -0,0 +1,37 @@
+
+#takes in a list, and returns only unique elements from it
+def unique(list):
+    return list(set(list))
+    
+#takes in a list converts it to unique values. Returns dictionary of keys representing the unique elements, and values containing the count
+def count_elements(arr):
+    if type(arr) == list:
+        unique_elements = list(set(arr))
+        count_dict = {}
+        for element in unique_elements:
+            count_dict[element] = 0
+        for element in arr:
+            if element in unique_elements:
+                count_dict[element] += 1
+        return count_dict
+    else:
+        return f'Function argument must be list, not {type(arr)}'
+
+#unpacks a nested list and counts the elements. Returns dictionary of keys representing the unique elements, and values containing the count
+def count_elements_nested(arr):
+    if type(arr) == list:
+        count_dict = {}
+        unnested = []
+        for elements in arr:
+            for element in elements:
+                unnested.append(element)
+        unique_elements = list(set(unnested))
+        for each in unique_elements:
+            count_dict[each] = 0
+        for element in unnested:
+            if element in unique_elements:
+                count_dict[element] += 1
+        return count_dict
+    else:
+        error = type(arr)
+        return f'Function argument must be list, not {error}'
