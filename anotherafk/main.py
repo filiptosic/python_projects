6e03a61676db4d44701da004f6eac5e5346e7ce5
Filip Tosic <tosicfilip3@gmail.com>
2026-04-06 20:26:00 -0600

    recommit

---
+++ anotherafk/main.py
@@ -0,0 +1,14 @@
+import pydoc
+
+import pyautogui
+import time
+import selenium
+# for i in range(20):
+#     time.sleep(20)
+#     pyautogui.typewrite('Hello world\n')
+
+import selenium.webdriver.options
+
+
+
+help('selemium.webdriver.options')
---
+++ useful_test2.py
@@ -0,0 +1,6 @@
+from useful_functions import count_elements, count_elements_nested
+
+arr = [['Pete', 'Pete', 'Pete'], ['harry', 'mary'], ['harry'], ['mary'], ['Pete', 'mary', 'harry'], 'skippy']
+number = 2
+
+print(count_elements_nested(arr))
